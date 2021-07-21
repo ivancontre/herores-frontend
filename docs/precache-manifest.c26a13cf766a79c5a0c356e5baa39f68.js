78108118344d65808fe4e0a6e014f6459f7aed9b
Ivan Contreras <ivanc.contre@gmail.com>
2021-07-21 13:04:22 -0400

    se modifica asset-manifest

---
+++ docs/precache-manifest.c26a13cf766a79c5a0c356e5baa39f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ea6c86b96031238b1dc9b84a312c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39fa4d24451608e36a10",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "39fa4d24451608e36a10",
+    "url": "/static/js/2.e17388d0.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.e17388d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15de84a1faba4ae6a74",
+    "url": "/static/js/main.47548afc.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
