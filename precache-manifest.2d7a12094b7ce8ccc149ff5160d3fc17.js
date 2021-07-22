88732ebb056fc66344ff2b93233b1eb4b179e1fe
Ivan Contreras <ivanc.contre@gmail.com>
2021-07-22 11:20:18 -0400

    Updates

--- precache-manifest.2d7a12094b7ce8ccc149ff5160d3fc17.js
+++ precache-manifest.2d7a12094b7ce8ccc149ff5160d3fc17.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0d87737eca182e5eee0e37add7d691ba",
+    "revision": "b2260cda27a35873fddc6f7f8f6c9188",
     "url": "/react-heroresapp/index.html"
   },
   {
-    "revision": "4cbfd041043b4cb57657",
+    "revision": "6ba1dbfa729ec6d0fa00",
     "url": "/react-heroresapp/static/css/2.2ce0d9b2.chunk.css"
   },
   {
-    "revision": "4cbfd041043b4cb57657",
-    "url": "/react-heroresapp/static/js/2.2cb3265d.chunk.js"
+    "revision": "6ba1dbfa729ec6d0fa00",
+    "url": "/react-heroresapp/static/js/2.6ab9a4e5.chunk.js"
   },
   {
     "revision": "f7e623707d9b37a3821ec4c3548a4309",
-    "url": "/react-heroresapp/static/js/2.2cb3265d.chunk.js.LICENSE.txt"
+    "url": "/react-heroresapp/static/js/2.6ab9a4e5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7fb5b1cec2fdd098d67a",
-    "url": "/react-heroresapp/static/js/main.5279565a.chunk.js"
+    "revision": "4ee2a1bcb4f31bfba718",
+    "url": "/react-heroresapp/static/js/main.6e05fbc0.chunk.js"
   },
   {
     "revision": "8938be15e6ac3adf7868",
