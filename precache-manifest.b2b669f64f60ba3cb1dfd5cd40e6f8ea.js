513359f115931f99461a782d524fd793fb13c4e8
Ivan Contreras <ivanc.contre@gmail.com>
2021-07-21 20:21:51 -0400

    Updates

---
+++ precache-manifest.b2b669f64f60ba3cb1dfd5cd40e6f8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e9dbdf3ef48e83a213d29c031c740c",
+    "url": "/react-heroresapp/index.html"
+  },
+  {
+    "revision": "39fa4d24451608e36a10",
+    "url": "/react-heroresapp/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "39fa4d24451608e36a10",
+    "url": "/react-heroresapp/static/js/2.e17388d0.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/react-heroresapp/static/js/2.e17388d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15de84a1faba4ae6a74",
+    "url": "/react-heroresapp/static/js/main.47548afc.chunk.js"
+  },
+  {
+    "revision": "8938be15e6ac3adf7868",
+    "url": "/react-heroresapp/static/js/runtime-main.403a4f89.js"
+  }
+]);
