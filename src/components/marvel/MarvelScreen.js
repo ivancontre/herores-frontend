2d24c1cba351d824c7265fe67b03ed89b0100c2b
Ivan Contreras <ivanc.contre@gmail.com>
2021-07-21 21:07:22 -0400

    Se agrega gh pages

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -4,7 +4,7 @@ import { HeroList } from '../heroes/HeroList';
 export const MarvelScreen = () => {
     return (
         <div>
-            <h1>MarvelScreen</h1>
+            <h1>Marvel Screen</h1>
 
             <hr />
 
